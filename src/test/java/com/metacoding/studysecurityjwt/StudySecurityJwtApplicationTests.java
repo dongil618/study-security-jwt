7e384b1737fc790ea58d5e97f9182b0e9dc6f959
dongil618 <rlaehddlf618@gmail.com>
2022-07-27 01:02:17 +0900

    프로젝트 생성

---
+++ src/test/java/com/metacoding/studysecurityjwt/StudySecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metacoding.studysecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
